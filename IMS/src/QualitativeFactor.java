680ed693cf4833700e52e3485abc3ef43003753a
sergioqueber <316341@via.dk>
2022-12-02 13:50:18 +0100

    Test

--- IMS/src/QualitativeFactor.java
+++ IMS/src/QualitativeFactor.java
@@ -1,3 +1,11 @@
-public class QualitativeFactor
+public class QualitativeFactor extends factor
 {
+  private QualitativeValue value;
+
+  QualitativeFactor(String name, int point, double weight, boolean type, QualitativeValue value){
+    super(name, point, weight, type);
+    this.value = value;
+  }
+
+
 }

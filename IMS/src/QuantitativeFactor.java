265db4852704734a4845c3840caf884b415d81dd
ericpintolo <ericpinto1@gmail.com>
2022-12-02 13:54:47 +0100

    Progress

--- IMS/src/QuantitativeFactor.java
+++ IMS/src/QuantitativeFactor.java
@@ -1,3 +1,50 @@
 public class QuantitativeFactor
+  extends Factor
 {
+  private double value;
+
+  public QuantitativeFactor(String name, int point,double weight, double value)
+  {
+    super(name,point, weight);
+    this.value=value;
+
+  }
+
+  public boolean equals(Object obj)
+    {
+      if (!(obj instanceof QuantitativeFactor))
+        return false;
+      QuantitativeFactor other= ( QuantitativeFactor) obj;
+
+      return  factor
+    }
+
+    public String toString()
+    {
+      return super.toString() + value;
+    }
+
+  public double getValue()
+  {
+    return value;
+  }
+
+  public void setValue(double value)
+  {
+    this.value = value;
+  }
+
+
+  public boolean isHigherThan(QuantitativeFactor quantitativeFactor)
+  {
+
+  }
+
+  public int assignPoint()
+  {
+
+  }
+
+
+
 }

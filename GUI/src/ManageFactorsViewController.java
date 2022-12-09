6855e3a1de004b4420994510c22c4ed11df15500
AnaVieira2610 <91956816+AnaVieira2610@users.noreply.github.com>
2022-12-09 14:53:22 +0100

    Factor Class

--- GUI/src/ManageFactorsViewController.java
+++ GUI/src/ManageFactorsViewController.java
@@ -19,77 +19,65 @@ public class ManageFactorsViewController
     private Region root;
     private ViewHandler viewHandler;
 
-    @FXML
-    private Button backButton;
+    @FXML private Button backButton;
 
-    @FXML
-    private Button deleteButton;
+    @FXML private Button deleteButton;
 
-    @FXML
-    private TableView <Integer> tableMA;
+    @FXML private TableView<Integer> tableMA;
 
-    @FXML
-    private TableColumn<Integer,String> factorColumnMA;
-    @FXML
-    private TableColumn<Integer,String> factorColumnCS;
+    @FXML private TableColumn<Integer, String> factorColumnMA;
+    @FXML private TableColumn<Integer, String> factorColumnCS;
 
-    @FXML
-    private Label labelCS;
+    @FXML private Label labelCS;
 
-    @FXML
-    private Label labelFactor;
+    @FXML private Label labelFactor;
 
-    @FXML
-    private Label labelMA;
+    @FXML private Label labelMA;
 
-    @FXML
-    private Label labelWeight;
+    @FXML private Label labelWeight;
 
-    @FXML
-    private Button saveCSbutton;
+    @FXML private Button saveCSbutton;
 
-    @FXML
-    private Button saveMAbutton;
+    @FXML private Button saveMAbutton;
 
-    @FXML
-    private TableView<Integer> tableCS;
+    @FXML private TableView<Integer> tableCS;
 
+    @FXML private TextField textFieldFactor;
 
-    @FXML
-    private TextField textFieldFactor;
+    @FXML private TextField textFieldWeight;
 
-    @FXML
-    private TextField textFieldWeight;
+    @FXML private TableColumn<Integer, Integer> weightColumnCS;
 
-    @FXML
-    private TableColumn<Integer, Integer> weightColumnCS;
+    @FXML private TableColumn<Integer, Integer> weightColumnMA;
 
-    @FXML
-    private TableColumn<Integer, Integer> weightColumnMA;
+    ArrayList MAFactor = new ArrayList();
+    ArrayList MAWeight = new ArrayList();
 
-  ArrayList MAFactor = new ArrayList();
-  ArrayList MAWeight = new ArrayList();
-@FXML public void addFactor(){
-   MAFactor.add(textFieldFactor.getText());
-   System.out.println(MAFactor);
-   factorColumnMA.setCellValueFactory(new PropertyValueFactory<>(textFieldFactor.getText()));
-   resetField();
+    @FXML public void addFactor()
+    {
+        MAFactor.add(textFieldFactor.getText());
+        System.out.println(MAFactor);
+        factorColumnMA.setCellValueFactory(new PropertyValueFactory<>(textFieldFactor.getText()));
+        resetField();
 
-}
-
-    @FXML public void addWeight(){
         MAWeight.add(textFieldWeight.getText());
         System.out.println(MAWeight);
         weightColumnMA.setCellValueFactory(new PropertyValueFactory<>(textFieldWeight.getText()));
         resetField();
 
     }
-public void resetField(){
-  textFieldFactor.setText("");
 
-}
+    public void resetField()
+    {
+        textFieldFactor.setText("");
+        textFieldWeight.setText((""));
+
+    }
+
+/*
 public void initialize(URL url, ResourceBundle resourceBundle){}
 factorColumnMA.setCellValueFactory(new PropertyValueFactory<>(textFieldFactor.getText()));
 weightColumnMA.setCellValueFactory(new PropertyValueFactory<>(textFieldWeight.getText()));
 table.setItems(get)
+}*/
 }

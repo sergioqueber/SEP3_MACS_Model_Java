9208e5b853928ac74f4c69763e550ab13f82c30f
sergioqueber <316341@via.dk>
2022-12-14 16:35:20 +0100

    Commit

--- IMS/src/View/IMSCalculatorViewController.java
+++ IMS/src/View/IMSCalculatorViewController.java
@@ -1,6 +1,6 @@
 package View;
 
-import Model.ModelIMS;
+import Model.*;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -37,37 +37,32 @@ public class IMSCalculatorViewController implements Initializable
     this.viewHandler2 = viewHandler2;
     this.root = root;
     this.model = model;
-  }
-
-  //XYChart.Series series1 = new XYChart.Series();
-  //XYChart.Series series2 = new XYChart.Series();
-
-
-  @Override public void initialize(URL location, ResourceBundle resources)
-  {
+    model.getList3().defineRange();
+    model.getList3().assignQuantitativePoints();
+    //model.getList3().assignQualitativePoints();
+    model.getList3().weightedMAPointsCalculation();
+    model.getList3().weightedCSPointsCalculation();
+    System.out.println(model.getList3().getCountries().get(0).getCompetitiveStrengthPunctuation());
+    System.out.println(model.getList3().getCountries().get(0).getMarketAttractivenessPunctuation());
+    System.out.println(model.getList3().getCountries().get(1).getCompetitiveStrengthPunctuation());
+    System.out.println(model.getList3().getCountries().get(1).getMarketAttractivenessPunctuation());
+    System.out.println(model.getList3().getCountries().get(1).getQuantitativeFactors());
     XYChart.Series series = new XYChart.Series();
     for (int i = 0; i < model.getList3().getCountries().size(); i++)
     {
       series.setName(model.getList3().getCountry(i).getName());
       series.getData().add(new XYChart.Data(
-          (model.getList3().getCountries().get(i).getCompetitiveStrengthPunctuation()),
-          (model.getList3().getCountries().get(i).getMarketAttractivenessPunctuation()),0.5));
+          model.getList3().getCountries().get(i).getCompetitiveStrengthPunctuation(),
+          model.getList3().getCountries().get(i).getMarketAttractivenessPunctuation(),0.2));
       bubbleChart.getData().add(series);
-
-
-/*
-   bubbleChart.getXAxis().setLabel("Competitive Strength");
-    bubbleChart.getYAxis().setLabel("Market Attractiveness");
-
-    series1.setName("Australia");
-    series1.getData().add(new XYChart.Data(3,4,0.5));
-
-    series2.setName("Japon");
-    series2.getData().add(new XYChart.Data(2,1,0.5));
-
-    bubbleChart.getData().addAll(series1, series2);
-*/
-
     }
+  }
+
+  @Override public void initialize(URL location, ResourceBundle resources)
+  {
+  }
+
+  @FXML public void back(){
+    viewHandler2.openView("Home Page 2");
   }
 }
